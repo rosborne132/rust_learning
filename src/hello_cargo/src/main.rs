e89c4c7af45cac218ec52d8fdf16e86aea4260ca
rosborne132 <ozborne132@gmail.com>
2021-05-30 20:52:51 -0700

    Add hello_world

---
+++ src/hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("{} days", 31);
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+        object="the lazy dog",
+        subject="the quick brown fox",
+        verb="jumps over");
+
+    println!("Hello, world!");
+}
